74c63674a4de143e566fa60f8081400fc0a4b8aa
Stefan Eiermann <github@ultraapp.de>
2020-01-01 12:10:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -14,22 +14,13 @@ try:
             "partitialajax",
         ])
     django.setup()
-
-    from django_nose import NoseTestSuiteRunner
-
+    import unittest
 except ImportError:
     raise ImportError("To fix this error, run: pip install -r requirements.txt")
 
 
 def run_tests(*test_args):
-    if not test_args:
-        test_args = ['tests']
-
-    test_runner = NoseTestSuiteRunner(verbosity=1)
-    failures = test_runner.run_tests(test_args)
-    if failures:
-        sys.exit(failures)
-
+    unittest.main()
 
 if __name__ == '__main__':
     run_tests(*sys.argv[1:])
